2a7a3dce04e7f3702ad1e80d08db66150c1a96e0
A O <ahmedothman8@hotmail.com>
2019-04-23 10:54:18 +0200

    add steve class

---
+++ src/gitwork/Steve.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitwork;
+
+/**
+ *
+ * @author ahmed
+ */
+public class Steve {
+    public void talk(){
+        System.out.println("hjojo holuuuo");
+    }
+}
